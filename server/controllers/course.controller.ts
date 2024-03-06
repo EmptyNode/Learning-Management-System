6a858d42d2f4401e2a2ab1569fbd280915923273
EmptyNode <96117153+EmptyNode@users.noreply.github.com>
2024-03-06 23:18:10 +0530

    course controller is added

---
+++ server/controllers/course.controller.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+import ErrorHandler from "../utils/ErrorHandler";
+import cloudinary from 'cloudinary';
+import { createCourse } from './../services/course.service';
+
+//upload course 
+export const uploadCourse = async (req: Request, res: Response, next: NextFunction) => {
+    try{
+        const data = req.body;
+        const thumbnail = data.thumbnail;
+        if(thumbnail){
+            const myCloud = await cloudinary.v2.uploader.upload(thumbnail, { 
+                folder: "course", 
+            });
+
+            data.thumbnail = {
+                public_id: myCloud.public_id,
+                url: myCloud.secure_url,
+            }
+        }
+        createCourse(data, res, next);
+    }catch(error: any){
+        return next(new ErrorHandler(error.message, 500));
+    }
+}    
