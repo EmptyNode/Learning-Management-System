f3da8c05e915d416be0ad50723eddd09953fc88d
EmptyNode <96117153+EmptyNode@users.noreply.github.com>
2024-03-16 23:56:32 +0530

    font style set, layout, theme provider basic set up complete

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -7,7 +7,9 @@ interface props{}
 const Page: FC<props> = (props)=>{
   return(
     <div>
-      <Heading title="Elearning" description="Elearning is a platform for students to learn and get help from teachers" keywords="Programming, MERN, Redux, Machine Learning" />
+      <Heading title="Elearning" 
+      description="Elearning is a platform for students to learn and get help from teachers" 
+      keywords="Programming, MERN, Redux, Machine Learning" />
     </div>
     )
 }
